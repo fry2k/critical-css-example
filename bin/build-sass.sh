caa4eb9d2e1ce398a91d1f5af26e3868a1ad8e3d
fry2k <fry2k@gmx.net>
2018-04-23 15:27:48 +0200

    * add watch task for build-sass * build sass file only with _header.scss

--- bin/build-sass.sh
+++ bin/build-sass.sh
@@ -2,3 +2,5 @@
 echo "build sass"
 node-sass src/sass/styles.scss dist/sass/styles.css
 node-sass src/sass/styles.critical.scss dist/sass/styles.critical.css
+
+concurrently --kill-others "node-sass --watch src/sass/styles.scss dist/sass/styles.css" "node-sass --watch src/sass/styles.critical.scss dist/sass/styles.critical.css"
